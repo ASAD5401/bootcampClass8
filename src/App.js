e15a7022afb08b7d47d4c605d5417f6f61fc47b1
ASAD5401 <asadali5401@gmail.com>
2021-09-11 19:25:33 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [repos,setRepos]=useState([{}])
+
+
+  useEffect(()=>{
+    async function getRepos(){
+      const response=await fetch("https://api.github.com/users/ASAD5401/repos")
+      const data=await response.json()
+      console.log(data)
+
+      setRepos(data)
+    }
+    getRepos();
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <ul>
+      {repos.map((repObj,index)=>{
+        return(<li key={index}>{repObj.name}</li>)
+      })}
+      </ul> 
+
+      
     </div>
   );
 }
